a1832ad0ffdeb0f1aee909bcaa26d05a829548ef
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2020-02-14 11:27:26 +0100

    Initial commit

--- provider-main.tf
+++ provider-main.tf
@@ -10,8 +10,4 @@ terraform {
 # Configure the Azure provider
 provider "azurerm" { 
   environment     = "public"
-  subscription_id = var.azure-subscription-id
-  client_id       = var.azure-client-id
-  client_secret   = var.azure-client-secret
-  tenant_id       = var.azure-tenant-id
 }
